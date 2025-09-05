386a5aa987ae282ed058b21e404d61ce1f000f4e
Robert MacRae <github@therealm18.com>
2025-09-05 00:22:57 -0300

    finally backed it up

---
+++ src/main/java/com/therealm18studios/oc2rlibrary/OC2RLibrary.java
@@ -0,0 +1,12 @@
+package com.therealm18studios.oc2rlibrary;
+
+import net.minecraftforge.fml.common.Mod;
+
+
+@Mod("oc2rlibrary")
+public class OC2RLibrary {
+    public static String MOD_ID = "oc2rlibrary";
+    public OC2RLibrary() {
+    }
+
+}
